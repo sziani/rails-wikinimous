ddc639fa6cf05c0a3109e9ff7c910c639dc4224b
Sami Ziani <zianisami0@gmail.com>
2021-02-10 10:50:49 +0100

    on going rails-wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do Article.create([title: Faker::Quotes::Shakespeare.king_richard_iii_quote, content: Faker::Quotes::Chiquito.sentence])
+end
